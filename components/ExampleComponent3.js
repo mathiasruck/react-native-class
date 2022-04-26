3e33d966ffc22b08fb910a9550fdd506edcbdc18
Mathias Ruckert <mathiasruck@gmail.com>
2022-04-26 21:08:12 +0100

    Componet lifecicle

--- components/ExampleComponent3.js
+++ components/ExampleComponent3.js
@@ -7,13 +7,32 @@ class ExampleComponent3 extends React.Component {
     this.state = {
       counter: 1,
     };
+    console.log("constructor...");
   }
 
   incrementCount(){
       const newState = {counter: this.state.counter + 1}
       this.setState(newState)
+      console.log("incrementCount...");
   }
+  componentDidMount(){
+    console.log("componentDidMount...");
+    return null;
+}
+  componentDidUpdate(){
+      console.log("componentDidUpdate...");
+      return null;
+  }
+  componentWillUpdate(){
+    console.log("componentWillUpdate...");
+    return null;
+}
+componentWillUnmount(){
+    console.log("componentWillUnmount...");
+    return null;
+}
   render() {
+    console.log("render...");
     return (
       <View>
         <Text style={{fontSize:40, textAlign:"center"}}>{this.state.counter}</Text>
